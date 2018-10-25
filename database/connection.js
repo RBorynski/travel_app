28880e6c78d5ff6f7f2447e583ef4812cbd8a1e1
Rafal Borynski <rafalborynski@Rafals-Air.fios-router.home>
2018-10-25 13:04:02 -0400

    made sure database file was alright

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,17 @@
-const pgp = require('pg-promise')();
+const pgp = require("pg-promise")();
 
 let db;
 
-if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
   db = pgp({
     // Fill in with your local database name
-    database: 'tourist_database',
+    database: "tourist_database",
     port: 5432,
-    host: 'localhost',
+    host: "localhost"
   });
-} else if (process.env.NODE_ENV === 'production') {
-    // Heroku will set this variable for you.
-    db = pgp(process.env.DATABASE_URL);
+} else if (process.env.NODE_ENV === "production") {
+  // Heroku will set this variable for you.
+  db = pgp(process.env.DATABASE_URL);
 }
 
-module.exports = db;
+module.exports = db; //
